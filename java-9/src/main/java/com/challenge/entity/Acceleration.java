61fd3f9ea1aad1fc4759db9c43d7a8bec87f0b36
cleytonRR <cleytonrodrigues69@live.com>
2020-05-12 15:19:12 -0300

    Define model Acceleration

--- java-9/src/main/java/com/challenge/entity/Acceleration.java
+++ java-9/src/main/java/com/challenge/entity/Acceleration.java
@@ -1,4 +1,31 @@
 package com.challenge.entity;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "acceleration")
 public class Acceleration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String slug;
+
+    @ManyToOne
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    public Acceleration() {
+    }
 }
