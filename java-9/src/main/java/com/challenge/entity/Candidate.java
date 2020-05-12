b63ada8bb2b862a95c3c6d46ee1095df6c49c0a4
cleytonRR <cleytonrodrigues69@live.com>
2020-05-12 15:37:04 -0300

    Define model Candidate

--- java-9/src/main/java/com/challenge/entity/Candidate.java
+++ java-9/src/main/java/com/challenge/entity/Candidate.java
@@ -1,4 +1,27 @@
 package com.challenge.entity;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "candidate")
 public class Candidate {
+
+    @EmbeddedId
+    private CandidateId id;
+
+    @Column(nullable = false)
+    private int status;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date created_at;
+
+    public Candidate() {
+    }
 }
