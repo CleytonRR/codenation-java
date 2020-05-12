2aa4f637499cd9dcb65aa77fea7c6da9add7f689
cleytonRR <cleytonrodrigues69@live.com>
2020-05-12 15:41:10 -0300

    Defined submission model

--- java-9/src/main/java/com/challenge/entity/Submission.java
+++ java-9/src/main/java/com/challenge/entity/Submission.java
@@ -1,4 +1,26 @@
 package com.challenge.entity;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name="submission")
 public class Submission {
+    @EmbeddedId
+    private SubmissionId id;
+
+    @Column(nullable = false)
+    private float score;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    public Submission() {
+    }
 }
