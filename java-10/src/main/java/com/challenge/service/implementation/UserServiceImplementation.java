8ff4d0871bb06c2aa08583423328f5b20dbac116
cleytonRR <cleytonrodrigues69@live.com>
2020-05-16 19:56:09 -0300

    Added method for implemenentationUser

--- java-10/src/main/java/com/challenge/service/implementation/UserServiceImplementation.java
+++ java-10/src/main/java/com/challenge/service/implementation/UserServiceImplementation.java
@@ -1,7 +1,9 @@
 package com.challenge.service.implementation;
 
 import com.challenge.entity.User;
+import com.challenge.repository.UserRepository;
 import com.challenge.service.interfaces.UserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,23 +11,27 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImplementation implements UserServiceInterface {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public Optional<User> findById(Long userId) {
-        return Optional.empty();
+        return this.userRepository.findById(userId);
     }
 
     @Override
     public List<User> findByAccelerationName(String name) {
-        return null;
+        return this.userRepository.findByAccelerationName(name);
     }
 
     @Override
     public List<User> findByCompanyId(Long companyId) {
-        return null;
+        return this.userRepository.findByCompanyId(companyId);
     }
 
     @Override
     public User save(User object) {
-        return null;
+        return this.userRepository.save(object);
     }
 }
