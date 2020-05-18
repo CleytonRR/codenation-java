e83fb3e0dc136a93910f774ca923c319150c3a14
cleytonRR <cleytonrodrigues69@live.com>
2020-05-17 21:58:08 -0300

    Create company repostiroy

---
+++ java-10/src/main/java/com/challenge/repository/CompanyRepository.java
@@ -0,0 +1,27 @@
+package com.challenge.repository;
+
+import com.challenge.entity.Company;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+    @Query(value = "select distinct company " +
+            "FROM company " +
+            "inner join candidate " +
+            "on company.id = candidate.company_id " +
+            "where candidate.acceleration_id = :acceleration", nativeQuery = true)
+    List<Company> findbyAccelerationId(@Param("companyId") Long companyId);
+
+    @Query(value = "select distinct company.* " +
+            "from company " +
+            "inner join candidate " +
+            "on company.id = candidate.company_id " +
+            "where candidate.user_id = :userId", nativeQuery = true)
+    List<Company> findByUserId(@Param("userId") Long userId);
+}
