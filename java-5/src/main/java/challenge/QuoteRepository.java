d56f1f023d8e11f540b4033da479eeb639bd13e4
cleytonRR <cleytonrodrigues69@live.com>
2020-05-24 17:36:44 -0300

    Implements service quote

--- java-5/src/main/java/challenge/QuoteRepository.java
+++ java-5/src/main/java/challenge/QuoteRepository.java
@@ -1,5 +1,11 @@
 package challenge;
 
-public interface QuoteRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuoteRepository extends JpaRepository<Quote, Integer> {
+    List<Quote> findQuotesByActor(String actor);
 
 }
--- java-5/src/main/java/challenge/QuoteServiceImpl.java
+++ java-5/src/main/java/challenge/QuoteServiceImpl.java
@@ -3,6 +3,9 @@ package challenge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
+
 @Service
 public class QuoteServiceImpl implements QuoteService {
 
@@ -11,12 +14,15 @@ public class QuoteServiceImpl implements QuoteService {
 
 	@Override
 	public Quote getQuote() {
-		return null;
+		return getRandomQuote(this.repository.findAll());
 	}
 
 	@Override
 	public Quote getQuoteByActor(String actor) {
-		return null;
+		return getRandomQuote(repository.findQuotesByActor(actor));
 	}
 
+	public Quote getRandomQuote(List<Quote> quotes) {
+		return quotes.get(new Random().nextInt(quotes.size()));
+	}
 }
