3dca572317190c204aeee195bcf764d292106ea4
cleytonRR <cleytonrodrigues69@live.com>
2020-05-31 22:47:53 -0300

    Created rules for endpoint /user

---
+++ java-12/src/main/java/com/challenge/security/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.challenge.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/user");
+    }
+}
